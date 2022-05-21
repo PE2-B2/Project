23ce194af8aab2c299e23723d459b585925ce257
junghun25 <dan05006@naver.com>
2022-05-21 15:24:36 +0900

    add model.py view.py run.py change filter.py extract.py IVAnalysis.py

--- src/mod.py
+++
@@ -1,9 +0,0 @@
-import os
-
-arr = os.listdir("C:/Users/main/Desktop/first/Project1/data")
-
-print("\n".join(arr))
-
-for (root, dirs, file) in os.walk("C:/Users/main/Desktop/first/Project1/data"):
-    for f in file:
-        print(f)
