948489584fe9d02a350fb1345ec12b58680d03a2
Nhat Tran <nhatdong.tran@googlemail.com>
2022-05-26 13:51:09 +0200

    add error warning and folder btn

--- src/gui.py
+++ src/gui.py
@@ -4,6 +4,9 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
 
 from src import view
+from src.filter import get_all_values
+
+import os
 
 
 class MainWindow(QMainWindow):
@@ -15,13 +18,14 @@ class MainWindow(QMainWindow):
     def setupUI(self):
         self.setWindowTitle("Program")
         self.setGeometry(100, 100, 550, 350)
+        self.setFixedSize(550, 350)
 
         self.setStyleSheet("background-color:white;")
 
-        self.titleLabel = QLabel("Software Name : ", self)
+        self.titleLabel = QLabel("Input : ", self)
         self.titleLabel.setFont(QFont('Arial bold', 15))
         self.titleLabel.move(50, 30)
-        self.titleLabel.resize(180, 20)
+        self.titleLabel.resize(180, 30)
 
         self.waferLabel = QLabel("Wafer : ", self)
         self.waferLabel.setFont(QFont('Arial', 10))
@@ -41,37 +45,40 @@ class MainWindow(QMainWindow):
         self.waferEdit = QLineEdit('', self)
         self.waferEdit.setStyleSheet("border-radius:5px;border: 1px solid black;text-align: center")
         self.waferEdit.move(170, 100)
-        self.waferEdit.resize(80, 20)
+        self.waferEdit.resize(100, 20)
 
         self.coordinateEdit = QLineEdit('', self)
         self.coordinateEdit.setStyleSheet("border-radius:5px;border: 1px solid black;text-align: center")
         self.coordinateEdit.move(170, 150)
-        self.coordinateEdit.resize(80, 20)
+        self.coordinateEdit.resize(100, 20)
 
         self.deviceEdit = QLineEdit('', self)
         self.deviceEdit.setStyleSheet("border-radius:5px;border: 1px solid black;text-align: center")
         self.deviceEdit.move(170, 200)
-        self.deviceEdit.resize(80, 20)
+        self.deviceEdit.resize(100, 20)
 
         self.showEdit = QCheckBox("Show Output", self)
-        self.showEdit.move(350, 190)
+        self.showEdit.move(350, 90)
         self.showEdit.toggle()
 
         self.saveEdit = QCheckBox("Save Output", self)
         self.saveEdit.move(350, 140)
         self.saveEdit.toggle()
 
-        self.label3 = QLabel('', self)
-        self.label3.move(50, 170)
-
         string = "background-color: grey;border: none;color: white;text-align: " \
                  "center;text-decoration: none;display: inline-block;font-size: 10px;margin: 4px 2px;cursor: pointer; "
 
-        self.btnSave = QPushButton("OK", self)
-        self.btnSave.setStyleSheet(string)
-        self.btnSave.resize(130, 30)
-        self.btnSave.move(380, 290)
-        self.btnSave.clicked.connect(self.btn_click)
+        self.processBtn = QPushButton("OK", self)
+        self.processBtn.setStyleSheet(string)
+        self.processBtn.resize(130, 30)
+        self.processBtn.move(380, 290)
+        self.processBtn.clicked.connect(self.btn_click)
+
+        self.folderBtn = QPushButton("Results folder", self)
+        self.folderBtn.setStyleSheet(string)
+        self.folderBtn.resize(130, 30)
+        self.folderBtn.move(220, 290)
+        self.folderBtn.clicked.connect(self.open_folder)
 
         grid_layout = QGridLayout()
         self.setLayout(grid_layout)
@@ -89,6 +96,22 @@ class MainWindow(QMainWindow):
         coordinates = self.coordinateEdit.text()
         device = self.deviceEdit.text()
 
-        start = time.time()
-        view.initMainView(wafer, coordinates, device, self.saveEdit.isChecked(), self.showEdit.isChecked())
-        print("time :", time.time() - start)
+        values = get_all_values()
+        try:
+            if wafer != '' and not all(elem in values[0] for elem in list(map(str, wafer.split()))):
+                QMessageBox.information(self, 'Message', str('Wafer ID doesnt exist!'))
+            elif coordinates != 'all' and not all(elem in values[1] for elem in list(map(str, coordinates.split()))):
+                QMessageBox.information(self, 'Message', str('Coordinates doesnt exist!'))
+            elif device != '' and not any(device in s for s in values[2]):
+                QMessageBox.information(self, 'Message', str('Device ID doesnt exist!'))
+            else:
+                start = time.time()
+                view.initMainView(wafer, coordinates, device, self.saveEdit.isChecked(), self.showEdit.isChecked())
+                print("time :", time.time() - start)
+        except ValueError as e:
+            QMessageBox.information(self, 'Error', str(e))
+
+    def open_folder(self):
+        path = "res"
+        path = os.path.realpath(path)
+        os.startfile(path)
